f0211f406f8adef46f7915bded2680517efb7b0e
masx200 <34191203+masx200@users.noreply.github.com>
2023-11-19 15:37:29 +0800

    Update grid-map.ts

--- path-planning/grid-map.ts
+++ path-planning/grid-map.ts
@@ -44,10 +44,13 @@ export class GridMap {
      * @param data - 一个二维数组，表示栅格数据。当栅格内有障碍物时值为1，并用黑色表示；无障碍物时值为0，并用白色表示。
      */
     constructor(
+        // 列数
         public column: number,
+        // 行数
         public row: number,
         /**
          当该栅格内有障碍物时赋值为1，并用黑色表示；无障碍物时赋值为0，并用白色表示。 */
+        // 数据，默认为大小为 column x row 的二维数组，所有元素初始化为0
         public data: number[][] = Array(column)
             .fill(0)
             .map(() => Array(row).fill(0))
