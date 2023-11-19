3a0e2ee3667b49ae21f2faa061c71de86e1011bb
masx200 <34191203+masx200@users.noreply.github.com>
2023-11-19 18:46:55 +0800

    Update Vec.test.ts

--- path-planning/Vec.test.ts
+++ path-planning/Vec.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { Vec } from "./Vec";
 
 
@@ -29,5 +29,56 @@ describe("Vec", () => {
         let v1 = new Vec(1, 2);
         let v2 = new Vec(2, 3);
         expect(v1.cos(v2)).toBeCloseTo(0.9922778767136677);
+    });
+});
+
+describe("Vec", () => {
+    let vec: Vec;
+
+    beforeEach(() => {
+        vec = new Vec(3, 4);
+    });
+
+    describe("#constructor()", () => {
+        it("should set the x and y properties correctly", () => {
+            expect(vec.x).toBe(3);
+            expect(vec.y).toBe(4);
+        });
+    });
+
+    describe("#length()", () => {
+        it("should calculate the vector length correctly", () => {
+            expect(vec.length()).toBeCloseTo(Math.sqrt(25));
+        });
+    });
+
+    describe("#siz()", () => {
+        it("should calculate the distance to the origin squared correctly", () => {
+            expect(vec.siz()).toBe(25);
+        });
+    });
+
+    describe("#dot()", () => {
+        let otherVec: Vec;
+
+        beforeEach(() => {
+            otherVec = new Vec(1, 2);
+        });
+
+        it("should calculate the dot product correctly", () => {
+            expect(vec.dot(otherVec)).toBe(11);
+        });
+    });
+
+    describe("#cos()", () => {
+        let otherVec: Vec;
+
+        beforeEach(() => {
+            otherVec = new Vec(1, 2);
+        });
+
+        it("should calculate the cosine of the angle between two vectors correctly", () => {
+            expect(vec.cos(otherVec)).toBeCloseTo(0.9807852804032304);
+        });
     });
 });
