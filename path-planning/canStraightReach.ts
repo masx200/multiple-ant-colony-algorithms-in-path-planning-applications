b4174b5e0a27a3259b939677ee7430605c4f8503
masx200 <34191203+masx200@users.noreply.github.com>
2023-11-22 22:43:43 +0800

    Update canStraightReach.ts

--- path-planning/canStraightReach.ts
+++ path-planning/canStraightReach.ts
@@ -18,11 +18,13 @@ export function canStraightReach(
     const [endCol, endRow] = end;
 
     // 获取地图的列和行
-    const [mapColumn, mapRow] = [grid.column, grid.row];
+    const mapColumn = grid.column,
+        mapRow = grid.row;
 
     // 判断起始点和终点是否在地图范围内
     // 判断起始点和终点是否在地图范围内
     if (
+        // 起始行号小于0或大于等于地图行数或起始列号小于0或大于等于地图列数
         startRow < 0 ||
         startRow >= mapRow ||
         startCol < 0 ||
@@ -43,8 +45,22 @@ export function canStraightReach(
     const pcd = getPathCoordinates(start, end);
     if (grid.isObstacle(startCol, startRow)) return false;
     if (grid.isObstacle(endCol, endRow)) return false;
+
     // 检查路径是否在网格内
     // 检查路径是否在网格内
+    for (let i = 0; i + 1 < pcd.length; i++) {
+        const [x1, y1] = pcd[i];
+        const [x2, y2] = pcd[i + 1];
+
+        // 如果相邻点之间的横坐标差为1且纵坐标差为1
+        if (Math.abs(x1 - x2) == 1 && Math.abs(y1 - y2) == 1) {
+            // 如果横坐标为x1的纵坐标位置和横坐标为x2的纵坐标位置都是障碍物，则返回false
+            if (grid.isObstacle(x1, y2) && grid.isObstacle(x2, y1))
+                return false;
+        }
+    }
+
+    // 如果路径上所有点的值都为0，则返回true，否则返回false
     if (
         pcd.every(([x, y]) => {
             return grid.data[x][y] === 0;
