2e727e1df211e6197dc407a77a38c2db77547471
masx200 <34191203+masx200@users.noreply.github.com>
2023-11-26 22:41:32 +0800

    Update PointsInsideAllConvexPolygons.ts

--- path-planning/PointsInsideAllConvexPolygons.ts
+++ path-planning/PointsInsideAllConvexPolygons.ts
@@ -20,17 +20,58 @@ export function PointsInsideAllConvexPolygons(
         for (let j = 0; j < n; j++) {
             if (grid.data[i][j] === 0 && !visited[i][j]) {
                 const stack: [number, number][] = [];
-                //let area = 0;
+
+                const pointsInConvexPolygons = new Set<string>();
                 stack.push([i, j]);
 
                 visited[i][j] = true;
+                pointsInConvexPolygons.add(JSON.stringify([i, j]));
                 while (stack.length) {
                     const [curI, curJ] = stack.pop() as [number, number];
-                    //  area++;
+
                     if (visited[curI][curJ] || grid.data[curI][curJ] === 1) {
                         continue;
                     }
+                    visited[curI][curJ] = true;
 
+                    const toBeDeleted = new Set<string>();
+                    let size = pointsInConvexPolygons.size;
+                    let count = 0;
+                    for (const point of pointsInConvexPolygons) {
+                        const pointArr = JSON.parse(point) as [number, number];
+
+                        if (
+                            visibleGridsMatrix[pointArr[0]][pointArr[1]][curI][
+                                curJ
+                            ]
+                        ) {
+                            count++;
+                        }
+
+                        if (
+                            dirs.every((dir) => {
+                                const x = pointArr[0] + dir[0];
+                                const y = pointArr[1] + dir[1];
+                                return (
+                                    x >= 0 &&
+                                    x < m &&
+                                    y >= 0 &&
+                                    y < n &&
+                                    grid.isFree(x, y)
+                                );
+                            })
+                        ) {
+                            toBeDeleted.add(point);
+                            ans.push(pointArr);
+                        }
+                    }
+
+                    for (const point of toBeDeleted) {
+                        pointsInConvexPolygons.delete(point);
+                    }
+                    if (count !== size) {
+                        continue;
+                    }
                     dirs.forEach((dir) => {
                         const x = curI + dir[0];
                         const y = curJ + dir[1];
@@ -47,7 +88,6 @@ export function PointsInsideAllConvexPolygons(
                         }
                     });
                 }
-                //  ans = Math.max(ans, area);
             }
         }
     }
