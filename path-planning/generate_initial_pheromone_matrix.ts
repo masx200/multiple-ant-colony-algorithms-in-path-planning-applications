214ba10959ed64aeab567feb0010cdf58933dc9e
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-09 15:09:53 +0800

    Update generate_initial_pheromone_matrix.ts

--- path-planning/generate_initial_pheromone_matrix.ts
+++ path-planning/generate_initial_pheromone_matrix.ts
@@ -6,28 +6,37 @@ import { GridMap } from "./grid-map";
 import { Point } from "./Point";
 
 
+/**
+ * 生成初始信息素矩阵
+ *
+ * @param grid 地图对象，包含地图的行数、列数以及障碍物信息
+ * @param start 起始点坐标
+ * @param end 结束点坐标
+ * @returns 初始信息素矩阵二维数组
+ */
 export function generate_initial_pheromone_matrix(
     grid: GridMap,
     start: Point,
     end: Point,
-) {
+): number[][] {
     // 计算地图的行数和列数
     const n = grid.row * grid.column;
+
     // 检查起始点是否在地图范围内
     assert.isAtLeast(start.x, 0);
     assert.isAtLeast(start.y, 0);
     assert.isAtMost(start.x, grid.column - 1);
     assert.isAtMost(start.y, grid.row - 1);
-    //assert.isAtAtMost(start.y, grid.row - 1);
+
     // 检查结束点是否在地图范围内
     assert.isAtLeast(end.x, 0);
     assert.isAtLeast(end.y, 0);
     assert.isAtMost(end.x, grid.column - 1);
     assert.isAtMost(end.y, grid.row - 1);
-    // assert.isAtAtMost(end.x, grid.column - 1);
-    //assert.isAtAtMost(end.y, grid.row - 1);
+
     // 获取地图的列数和行数
     const { column, row } = grid;
+
     // 初始化一个二维数组，用于存储初始信息素矩阵
     const res: number[][] = Array(column)
         .fill(0)
@@ -40,6 +49,7 @@ export function generate_initial_pheromone_matrix(
             if (grid.isObstacle(i, j)) {
                 continue;
             }
+
             // 计算当前点到起始点和结束点的距离
             const distance =
                 EuclideanDistance(start.x, start.y, i, j) +
@@ -53,16 +63,20 @@ export function generate_initial_pheromone_matrix(
                 ].flat(),
                 (item) => JSON.stringify(item),
             );
+
             // 计算当前点是障碍物的次数
             const obstacleCount = pcds.filter((item) =>
                 grid.isObstacle(item[0], item[1]),
             ).length;
+
             // 计算当前点是空地的次数
             const freecount = pcds.length - obstacleCount;
+
             // 计算当前点的信息素矩阵
             res[i][j] = 1 / n / distance ** (freecount / pcds.length);
         }
     }
+
     // 返回初始信息素矩阵
     return res;
 }
