e7f8ebd4ccb474abc2793de0f7902002a4dcd82e
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-10 23:23:52 +0800

    Update RangeModule.test.ts

--- path-planning/RangeModule.test.ts
+++ path-planning/RangeModule.test.ts
@@ -1,28 +1,29 @@
 import { test } from "vitest";
-import { RangeModule } from "./RangeModule";
 import { assert } from "vitest";
+import { RangeModule } from "./RangeModule";
+
 
 test("RangeModule", () => {
     const rangeModule = new RangeModule(10, 40);
     rangeModule.addRange(10, 20);
-    console.log(JSON.stringify(rangeModule));
+    // console.log(JSON.stringify(rangeModule));
     let availableRanges = rangeModule.getAvailableRanges();
-    console.log(availableRanges); // 输出 [[10, 20]]
+    // console.log(availableRanges); // 输出 [[10, 20]]
     assert.deepEqual(availableRanges, [[10, 20]]);
     // debugger;
     rangeModule.addRange(30, 40);
-    console.log(JSON.stringify(rangeModule));
+    // console.log(JSON.stringify(rangeModule));
     availableRanges = rangeModule.getAvailableRanges();
-    console.log(availableRanges); // 输出 [[10, 20], [30, 40]]
+    // console.log(availableRanges); // 输出 [[10, 20], [30, 40]]
     assert.deepEqual(availableRanges, [
         [10, 20],
         [30, 40],
     ]);
     // debugger;
     rangeModule.removeRange(25, 35);
-    console.log(JSON.stringify(rangeModule));
+    // console.log(JSON.stringify(rangeModule));
     availableRanges = rangeModule.getAvailableRanges();
-    console.log(availableRanges); // 输出 [[10, 20], [35, 40]]
+    // console.log(availableRanges); // 输出 [[10, 20], [35, 40]]
     assert.deepEqual(availableRanges, [
         [10, 20],
         [35, 40],
