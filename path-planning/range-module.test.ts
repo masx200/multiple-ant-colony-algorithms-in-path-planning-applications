b127ea706c824b7ccc0c675e6c3ae0b7f353f3f7
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-11 11:00:17 +0800

    Update range-module.test.ts

--- path-planning/range-module.test.ts
+++ path-planning/range-module.test.ts
@@ -10,7 +10,7 @@ describe("RangeModule", () => {
         //     expect(rangeModule.root.leftChild?.end).toBe(10);
         //     expect(rangeModule.root.rightChild?.start).toBe(20);
         //     expect(rangeModule.root.rightChild?.end).toBe(100);
-        const rangeModule = new RangeModule(0, 10 ** 9);
+        const rangeModule = new RangeModule(10, 20);
         rangeModule.addRange(10, 20);
         rangeModule.removeRange(14, 16);
         assert(rangeModule.queryRange(10, 14)); // 返回 true （区间 [10, 14) 中的每个数都正在被跟踪）
@@ -18,7 +18,7 @@ describe("RangeModule", () => {
         assert(rangeModule.queryRange(16, 17)); //返回 true （尽管执行了删除操作，区间 [16, 17) 中的数字 16 仍然会被跟踪）
     });
     test("queryRange", () => {
-        const rangeModule = new RangeModule(0, 100);
+        const rangeModule = new RangeModule(5, 20);
         rangeModule.addRange(5, 15);
         const availableRanges = rangeModule.getAvailableRanges();
         // console.log(availableRanges);
