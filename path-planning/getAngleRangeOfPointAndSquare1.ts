fd688f9c5328cd8285f3f28182c940bef9c6febe
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-12 23:19:56 +0800

    Update getAngleRangeOfPointAndSquare1.ts

--- path-planning/getAngleRangeOfPointAndSquare1.ts
+++ path-planning/getAngleRangeOfPointAndSquare1.ts
@@ -13,18 +13,18 @@ export function getAngleRangeOfPointAndSquare1(
     x: number,
     y: number,
 ): [number, number] {
+    const four_edges = [
+        [x - 0.5, y + 0.475],
+        [x - 0.475, y + 0.5],
+        [x + 0.475, y + 0.5],
+        [x + 0.5, y + 0.475],
+        [x + 0.5, y - 0.475],
+        [x + 0.475, y - 0.5],
+        [x - 0.475, y - 0.5],
+        [x - 0.5, y - 0.475],
+    ] as Array<[number, number]>;
     // 计算四个角度值
-    const angles = [
-        //格子缩小到95%,为了减小误差
-        Math.atan2(y + 0.5 * 0.95 - b, x + 0.5 - a),
-        Math.atan2(y + 0.5 - b, x - 0.5 * 0.95 - a),
-        Math.atan2(y - 0.5 * 0.95 - b, x + 0.5 - a),
-        Math.atan2(y - 0.5 - b, x - 0.5 * 0.95 - a),
-        Math.atan2(y + 0.5 * 0.95 - b, x + 0.5 - a),
-        Math.atan2(y + 0.5 - b, x - 0.5 * 0.95 - a),
-        Math.atan2(y - 0.5 * 0.95 - b, x + 0.5 - a),
-        Math.atan2(y - 0.5 - b, x - 0.5 * 0.95 - a),
-    ];
+    const angles = four_edges.map(([i, j]) => Math.atan2(j - b, i - a));
 
     // 找到最大值和最小值
     const max = Math.max(...angles);
