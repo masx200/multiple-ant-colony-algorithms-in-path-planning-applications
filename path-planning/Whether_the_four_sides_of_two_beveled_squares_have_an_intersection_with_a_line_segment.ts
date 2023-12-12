9329e75f5e3828cf68d6d0f0851dc4c256766530
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-12 23:10:26 +0800

    [x - 0.5, y + 0.475],             [x - 0.475, y + 0.5],             [x + 0.475, y + 0.5],             [x + 0.5, y + 0.475],             [x + 0.5, y - 0.475],             [x + 0.475, y - 0.5],             [x - 0.475, y - 0.5],             [x - 0.5, y - 0.475],

--- path-planning/Whether_the_four_sides_of_two_beveled_squares_have_an_intersection_with_a_line_segment.ts
+++ path-planning/Whether_the_four_sides_of_two_beveled_squares_have_an_intersection_with_a_line_segment.ts
@@ -3,6 +3,7 @@
 import { assert } from "vitest";
 import { robustsegmentintersect } from "../cross-points/robust-segment-intersect";
 import { GridMap } from "./grid-map";
+import { cycle_route_to_segments } from "../functions/cycle_route_to_segments";
 
 // 引入自定义模块robust-segment-intersect，这个模块应该包含了一个函数robustsegmentintersect，用于计算两条线段是否相交
 
@@ -27,28 +28,18 @@ export function Whether_the_four_sides_of_two_beveled_squares_have_an_intersecti
         if (gird.isFree(x, y)) return false;
         // 定义一个四边形的四个顶点坐标，这个四边形是以当前点为中心，边长为1的正方形，顶点坐标通过当前点的坐标和正方形的方向计算得出
         const four_edges = [
-            [
-                /* 忽略四个角的坐标 */
-                [x - 0.5, y - 0.5], // 第一个顶点坐标
-                [x + 0.5, y - 0.5], // 第二个顶点坐标
-            ],
-            [
-                //长宽缩到95%
-                [x + 0.5, y - 0.5], // 第三个顶点坐标
-                [x + 0.5, y + 0.5], // 第四个顶点坐标
-            ],
-            [
-                //处理斜45度的情况
-                [x + 0.5, y + 0.5], // 第五个顶点坐标
-                [x - 0.5, y + 0.5], // 第六个顶点坐标
-            ],
-            [
-                [x - 0.5, y + 0.5], // 第七个顶点坐标
-                [x - 0.5, y - 0.5], // 第八个顶点坐标
-            ],
-        ] as const; // 将数组标记为只读，防止后续更改这个数组的内容
+            [x - 0.5, y + 0.475],
+            [x - 0.475, y + 0.5],
+            [x + 0.475, y + 0.5],
+            [x + 0.5, y + 0.475],
+            [x + 0.5, y - 0.475],
+            [x + 0.475, y - 0.5],
+            [x - 0.475, y - 0.5],
+            [x - 0.5, y - 0.475],
+        ] as Array<[number, number]>; // 将数组标记为只读，防止后续更改这个数组的内容
+        const segments = cycle_route_to_segments(four_edges);
         // 使用数组的some方法，遍历这个四边形的四个边，检查是否存在一条边与第二个线段相交
-        return four_edges.some(([point1, point2]) => {
+        return segments.some(([point1, point2]) => {
             // 使用自定义函数robustsegmentintersect，检查一条边是否与第二个线段相交，如果相交返回true，否则返回false
             return robustsegmentintersect(
                 [point1[0], point1[1]], // 第一条边的起点坐标
