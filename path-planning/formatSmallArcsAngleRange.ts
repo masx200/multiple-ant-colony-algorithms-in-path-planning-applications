441de96ba8a6269b8933f405c9a4d1bc9a1187ef
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-13 14:23:06 +0800

    测试

--- path-planning/formatSmallArcsAngleRange.ts
+++ path-planning/formatSmallArcsAngleRange.ts
@@ -1,6 +1,19 @@
+/**
+ * 这个函数用于处理角度范围，将角度转换为弧度并规范化范围。如果角度范围跨过π，则返回两个数组，第一个数组包含最大值和π，第二个数组包含−π和最小值。否则，只返回一个数组，包含最小值和最大值。
+ * @param angles
+ * @returns
+ */
 export function formatSmallArcsAngleRange(
     angles: number[],
 ): [number, number][] {
+    angles = angles.map((a) => {
+        if (a >= -Math.PI && a < Math.PI) return a;
+        a = a % (2 * Math.PI);
+        if (a < 0) {
+            a += 2 * Math.PI;
+        }
+        return a >= Math.PI ? a - 2 * Math.PI : a;
+    });
     const max = Math.max(...angles);
     const min = Math.min(...angles);
     // console.log({
