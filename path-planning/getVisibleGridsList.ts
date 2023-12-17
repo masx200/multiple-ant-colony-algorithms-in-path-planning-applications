7f1dc927dd4582b1d52fe2d6b5bc5b31dbd19e5e
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-17 16:15:49 +0800

    getVisibleGridsList,get_length_of_one_route_on_grid_map

---
+++ path-planning/getVisibleGridsList.ts
@@ -0,0 +1,47 @@
+import { findVisibleGridsBFS } from "./findVisibleGridsBFS";
+import { GridMap } from "./grid-map";
+
+/**
+ * 生成一个布尔类型的四维数组，表示网格地图中每个格子与其可视的格子之间的连通关系
+ *
+ * @param grid - GridMap类型，表示网格地图对象
+ * @returns 布尔类型的四维数组，表示网格地图中每个格子与其可视的格子之间的连通关系
+ */
+export function getVisibleGridsList(
+    grid: GridMap,
+): Array<[number, number]>[][] {
+    // 定义一个四维数组 result，用于存储最终的结果
+    const result: Array<[number, number]>[][] = [];
+    // 遍历 grid.data 数组
+    for (let i = 0; i < grid.data.length; ++i) {
+        // 定义一个三维数组 row，用于存储当前行的数据
+        const row: Array<[number, number]>[] = [];
+        // 遍历 grid.data[i] 数组
+        for (let j = 0; j < grid.data[i].length; ++j) {
+            // 定义一个二维数组 col，用于存储当前列的数据
+            const col: Array<[number, number]> = [];
+
+            // 将 col 数组推入 row 数组
+            row.push(col);
+        }
+        // 将 row 数组推入 result 数组
+        result.push(row);
+    }
+
+    // 遍历 grid.data 数组
+    for (let i = 0; i < grid.data.length; ++i) {
+        // 遍历 grid.data[i] 数组
+        for (let j = 0; j < grid.data[i].length; ++j) {
+            // 如果当前位置是自由位置（isFree 方法判断）
+            if (grid.isFree(i, j)) {
+                // 调用 findVisibleGridsBFS 方法找到可见的格子（VisibleGrids）
+                const VisibleGrids = findVisibleGridsBFS(i, j, grid);
+                // 遍历 VisibleGrids 数组
+                result[i][j] = VisibleGrids;
+            }
+        }
+    }
+
+    // 返回最终结果 result
+    return result;
+}
---
+++ path-planning/search_one_route_on_grid_map.ts
@@ -0,0 +1,12 @@
+import { Point } from "./Point";
+import { GridMap } from "./grid-map";
+
+export function search_one_route_on_grid_map(
+    route: [number, number][],
+    grid: GridMap,
+    start: Point,
+    end: Point,
+    PheromoneMatrix: number[][],
+): number[] {
+    throw new Error("Not Implemented");
+}
