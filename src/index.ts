ef554d3fc01274f89cbd90cb568451668b8298a3
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-18 21:40:44 +0800

    service worker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import "./service-worker.ts";
 import "core-js/stable/array/at";
+
 import { appcontainer } from "./appcontainer";
 import { app } from "./main";
+
 app.config.errorHandler = (e: any) => {
     typeof alert === "function" &&
         alert?.(
@@ -16,27 +19,6 @@ app.config.errorHandler = (e: any) => {
     });
 };
 app.mount(appcontainer);
-!(async () => {
-    //@ts-ignore
-    if (process.env.NODE_ENV === "production") {
-        //@ts-ignore
-        const { registerSW } = await import("virtual:pwa-register");
-
-        if (
-            location.hostname !== "localhost" &&
-            "127.0.0.1" !== location.hostname
-        ) {
-            "serviceWorker" in navigator &&
-                (function () {
-                    const updateSW = registerSW({
-                        onNeedRefresh() {},
-                        onOfflineReady() {},
-                    });
-                    updateSW(true).catch(() => {});
-                })();
-        }
-    }
-})();
 self.addEventListener("unhandledrejection", (e) => {
     // console.log(self);
     // console.error(e);
---
+++ src/service-worker.ts
@@ -0,0 +1,21 @@
+!(async () => {
+    //@ts-ignore
+    if (process.env.NODE_ENV === "production") {
+        //@ts-ignore
+        const { registerSW } = await import("virtual:pwa-register");
+
+        if (
+            location.hostname !== "localhost" &&
+            "127.0.0.1" !== location.hostname
+        ) {
+            "serviceWorker" in navigator &&
+                (function () {
+                    const updateSW = registerSW({
+                        onNeedRefresh() {},
+                        onOfflineReady() {},
+                    });
+                    updateSW(true).catch(() => {});
+                })();
+        }
+    }
+})();
