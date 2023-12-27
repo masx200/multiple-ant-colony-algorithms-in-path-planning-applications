9c4588132ef0d6dde92822fbd3b9bb1d6c8f799c
masx200 <masx200@qq.com>
2023-12-28 00:58:51 +0800

    测试不可到达的

--- path-planning/search-drawGridMap-test.test.ts
+++ path-planning/search-drawGridMap-test.test.ts
@@ -61,3 +61,44 @@ import { GridMapFromArray } from "./GridMapFromArray";
 import { PointsInsideAllConvexPolygons } from "./PointsInsideAllConvexPolygons";
 import { generate_initial_pheromone_matrix } from "./generate_initial_pheromone_matrix";
 import { DefaultOptions } from "../src/default_Options";
+import 不可到达的测试 from "./不可到达的测试.json";
+test("search-drawGridMap-test", () => {
+    const start = new Point(1, 21);
+    const end = new Point(22, 1);
+
+    const gridmap = GridMapFromArray(不可到达的测试);
+    const visibleGridsList = getVisibleGridsList(gridmap);
+    const visibleGridsMatrix = VisibleGridsMatrix(visibleGridsList);
+    const pointsInsideAllConvexPolygons = new Set(
+        [...PointsInsideAllConvexPolygons(gridmap, visibleGridsMatrix)].map(
+            (a) => a[0] * gridmap.row + a[1],
+        ),
+    );
+    const PheromoneMatrix = generate_initial_pheromone_matrix(
+        gridmap,
+        start,
+        end,
+    );
+    const PheromoneZeroMatrix = structuredClone(PheromoneMatrix);
+    const q0_Path_selection_parameters = 0.8;
+    //console.log(route.value);
+    //const visibleGridsMatrix=VisibleGridsMatrix(visibleGridsList)
+    const path = search_one_route_on_grid_map(
+        gridmap,
+        start,
+        end,
+        PheromoneMatrix,
+        visibleGridsList,
+        visibleGridsMatrix,
+        pointsInsideAllConvexPolygons,
+        DefaultOptions.alpha_zero,
+        DefaultOptions.beta_zero,
+        q0_Path_selection_parameters,
+        PheromoneZeroMatrix,
+        DefaultOptions.local_pheromone_volatilization_coefficient,
+        DefaultOptions.global_pheromone_volatilization_coefficient,
+    );
+    console.log(path);
+
+    assert(path.length == 0);
+});
