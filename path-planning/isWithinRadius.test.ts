58054d1f9730bf4d0afc54b0414e34939810d331
masx200 <34191203+masx200@users.noreply.github.com>
2023-12-30 14:39:53 +0800

    Update isWithinRadius.test.ts

--- path-planning/isWithinRadius.test.ts
+++ path-planning/isWithinRadius.test.ts
@@ -1,29 +1,30 @@
-import chai from "chai";
+import  {assert}from "chai";
 import { describe, it } from "vitest";
 import { isWithinRadius } from "./isWithinRadius";
 import { Point } from "./Point";
+
 
 describe("isWithinRadius 函数测试", () => {
     it("正常情况下的测试 - 在半径内的点", () => {
         let p1 = new Point(1, 1);
         let radiusSquared = 4;
-        chai.assert(isWithinRadius(p1, radiusSquared));
+        assert(isWithinRadius(p1, radiusSquared));
     });
 
     it("正常情况下的测试 - 在半径外的点", () => {
         let p1 = new Point(2, 2);
         let radiusSquared = 1;
-        chai.assert.isFalse(isWithinRadius(p1, radiusSquared));
+        assert.isFalse(isWithinRadius(p1, radiusSquared));
     });
     it("正常情况下的测试 - 在半径内的点", () => {
         let p1 = new Point(1, 1);
         let radiusSquared = 2;
-        chai.assert(isWithinRadius(p1, radiusSquared));
+        assert(isWithinRadius(p1, radiusSquared));
     });
 
     it("正常情况下的测试 - 在半径外的点", () => {
         let p1 = new Point(2, 2);
         let radiusSquared = 8;
-        chai.assert(isWithinRadius(p1, radiusSquared));
+        assert(isWithinRadius(p1, radiusSquared));
     });
 });
