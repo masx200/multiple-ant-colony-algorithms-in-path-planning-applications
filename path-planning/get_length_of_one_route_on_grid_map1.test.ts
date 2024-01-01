a1f167be4b79bb9f07ff636619e826a29ccd2d1b
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-01 21:10:47 +0800

    Create get_length_of_one_route_on_grid_map1.test.ts

---
+++ path-planning/get_length_of_one_route_on_grid_map1.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, it } from "vitest";
+import { get_length_of_one_route_on_grid_map } from "./get_length_of_one_route_on_grid_map";
+import { GridDistanceMatrix } from "./Grid-distance-matrix";
+
+describe("get_length_of_one_route_on_grid_map", () => {
+    it("should return Infinity if the route is empty", () => {
+        const route: [number, number][] = [];
+        const DistanceMatrix: number[][][][] = [];
+        const result = get_length_of_one_route_on_grid_map(
+            route,
+            DistanceMatrix,
+        );
+        expect(result).toBe(Infinity);
+    });
+
+    it("should return Infinity if the route has only one point", () => {
+        const route: [number, number][] = [[0, 0]];
+        const DistanceMatrix: number[][][][] = [];
+        const result = get_length_of_one_route_on_grid_map(
+            route,
+            DistanceMatrix,
+        );
+        expect(result).toBe(Infinity);
+    });
+
+    it("should calculate the length of the route correctly", () => {
+        const route: [number, number][] = [
+            [0, 0],
+            [1, 1],
+            [2, 2],
+            [4, 2],
+            [5, 1],
+            [3, 3],
+        ];
+        const DistanceMatrix: number[][][][] = GridDistanceMatrix(6, 4);
+        const result = get_length_of_one_route_on_grid_map(
+            route,
+            DistanceMatrix,
+        );
+        expect(result).toBe(9.071067811865476);
+    });
+
+    it("should calculate the length of the route correctly", () => {
+        const route: [number, number][] = [
+            [0, 0],
+
+            [5, 3],
+        ];
+        const DistanceMatrix: number[][][][] = GridDistanceMatrix(6, 4);
+        const result = get_length_of_one_route_on_grid_map(
+            route,
+            DistanceMatrix,
+        );
+        expect(result).toBe(5.830951894845301);
+    });
+});
