c6cafefb16a783a26c5ea6603039f537fd14482c
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-07 17:54:24 +0800

    appcom

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import "../functions/echarts-line";
 import "./style.css";
 
 import { createApp, h } from "vue";
-import appcom from "../path-planning/search-drawGridMap-test-random-reverse.vue";
+// import appcom from "../path-planning/show-drawGridMap-PointsInsideAllConvexPolygons-test-3.vue";
+import appcom from "./app-com.vue"
+
+
+// import appcom from "../path-planning/search-drawGridMap-test-random-reverse.vue";
 
 // import appcom from "../path-planning/search-drawGridMap-test-greedy.vue";
 
 // import appcom from "../path-planning/search-drawGridMap-test.vue";
 
-// import appcom from "../path-planning/show-drawGridMap-PointsInsideAllConvexPolygons-test-3.vue";
-
-document.title = "ant-colony-algorithm-for-multiple-populations";
+document.title = "path-planning-ant-colony-algorithm-for-multiple-populations";
 
 export const app = createApp({
     render() {
