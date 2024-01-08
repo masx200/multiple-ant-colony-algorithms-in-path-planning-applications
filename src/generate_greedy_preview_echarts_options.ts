74ff40716b0056b5fab6581131873b8d8bbfb397
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-08 22:16:51 +0800

    Update generate_greedy_preview_echarts_options.ts

--- src/generate_greedy_preview_echarts_options.ts
+++ src/generate_greedy_preview_echarts_options.ts
@@ -1,20 +1,17 @@
-import { ECBasicOption } from "echarts/types/dist/shared";
-import { Ref } from "vue";
-// import {
-ECOption
-} from "../functions/echarts-line";
-import { NodeCoordinates } from "../functions/NodeCoordinates";
-import { DefaultOptions } from "./default_Options";
-import { get_options_of_random_greedy_of_tsp } from "./get_options_of_random_greedy_of_tsp";
-import { get_distance_round } from "./set_distance_round";
-
+import { DefaultOptions } from './default_Options';
+import { ECBasicOption } from 'echarts/types/dist/shared';
+import { ECOption } from '../functions/echarts-line';
+import { get_distance_round } from './set_distance_round';
+import { get_options_of_random_greedy_of_tsp } from './get_options_of_random_greedy_of_tsp';
+import { NodeCoordinates } from '../functions/NodeCoordinates';
+import { Ref } from 'vue';
 
 export async function generate_greedy_preview_echarts_options({
     selected_node_coordinates,
-    node_coordinates
+    node_coordinates,
 }: {
     selected_node_coordinates: Ref<NodeCoordinates | undefined>;
-    node_coordinates:()=>Promise<NodeCoordinates>
+    node_coordinates: () => Promise<NodeCoordinates>;
 }): Promise<ECBasicOption & ECOption> {
     // const element = selecteleref.value;
     // const node_coordinates = TSP_cities_map.get(element?.value || "");
