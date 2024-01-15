18b0d17d472856911018d1b39e7c121e597d9de4
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-15 20:11:47 +0800

    Update is_intersection_filter_with_cycle_route_old.ts

--- cross-points/is_intersection_filter_with_cycle_route_old.ts
+++ cross-points/is_intersection_filter_with_cycle_route_old.ts
@@ -1,10 +1,17 @@
 import { combinations } from "combinatorial-generators";
 
 import { robustsegmentintersect } from "../cross-points/robust-segment-intersect";
-import { cycle_route_to_segments } from "../functions/cycle_route_to_segments";
 import { haverepetitions } from "../functions/haverepetitions";
 import { assert_true } from "../test/assert_true";
+import { not_cycle_route_to_segments } from "../functions/not_cycle_route_to_segments";
+import { oneDimensionToTwoDimensions } from "../path-planning/oneDimensionToTwoDimensions";
 
+/**
+ * 检查是否存在交叉过滤中的不循环路径
+ * @param {number[]} cycle_route - 循环路由
+ * @param {number[][]} node_coordinates - 节点坐标
+ * @returns {boolean} - 是否存在交叉过滤中的循环路由
+ */
 export function is_intersection_filter_with_cycle_route_old({
     cycle_route,
     node_coordinates,
@@ -16,15 +23,15 @@ export function is_intersection_filter_with_cycle_route_old({
     const count_of_nodes = node_coordinates.length;
     assert_true(count_of_nodes > 1);
     assert_true(cycle_route.length >= 2);
-    const cyclesegments = cycle_route_to_segments(cycle_route);
-
+    const cyclesegments = not_cycle_route_to_segments(cycle_route);
+    const n = node_coordinates[0].length;
     for (const [[left1, left2], [right1, right2]] of combinations(
         cyclesegments,
         2,
     )) {
         if (!haverepetitions([left1, right1, left2, right2])) {
             const intersectparameters = [left1, left2, right1, right2].map(
-                (node) => node_coordinates[node],
+                (node) => oneDimensionToTwoDimensions(node, n),
             );
             if (
                 robustsegmentintersect(
