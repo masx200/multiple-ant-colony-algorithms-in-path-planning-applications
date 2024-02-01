b8fd624d8fb0de874b8b222da17bd977fd030a07
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-01 18:22:31 +0800

    Update run_greedy_once_thread_with_time.ts

--- functions/run_greedy_once_thread_with_time.ts
+++ functions/run_greedy_once_thread_with_time.ts
@@ -1,5 +1,12 @@
 // import { Greedy_algorithm_to_solve_tsp_with_selected_start_pool } from "../src/Greedy_algorithm_to_solve_tsp_with_selected_start_pool";
 import { Greedy_solve_tsp_with_selected_start_length_time_ms } from "../functions/Greedy-solve-tsp-with-selected-start-length-time-ms";
+/**
+ * 使用贪婪算法计算一次带有时间限制的旅行商问题的最优解。
+ * @param node_coordinates 节点的坐标
+ * @param start 起始节点的索引
+ * @param end 终止节点的索引
+ * @returns 返回一个Promise对象，包含最优解的长度、路径和耗时（以毫秒为单位）
+ */
 export async function run_greedy_once_thread_with_time({
     node_coordinates,
     start,
