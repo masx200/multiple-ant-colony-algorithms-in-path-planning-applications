282ed95645e2735783fd6fae435b11f8a48e7943
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-01 18:30:54 +0800

    Update pick-next-node-Roulette.ts

--- functions/pick-next-node-Roulette.ts
+++ functions/pick-next-node-Roulette.ts
@@ -3,19 +3,23 @@ import { PickNextNodeRouletteOptions } from "./PickNextNodeRouletteOptions";
 import { pickRandomOne } from "./pickRandomOne";
 import { SharedOptions } from "./SharedOptions";
 
+/**
+ * 从给定的选项中选择下一个节点的轮盘赌法
+ * @param options - 选择下一个节点的选项
+ * @returns {number} - 选择的下一个节点的编号
+ */
 export function picknextnodeRoulette(
     options: PickNextNodeRouletteOptions & {
         get_convergence_coefficient: () => number;
     } & SharedOptions,
 ): number {
     const {
-        alpha_zero,
-
-        beta_zero,
-        getpheromone,
-        getdistancebyserialnumber,
-        currentnode,
-        availablenextnodes,
+        alpha_zero, // 轮盘赌法的alpha参数
+        beta_zero, // 轮盘赌法的beta参数
+        getpheromone, // 获取pheromone值的函数
+        getdistancebyserialnumber, // 根据序列号获取距离的函数
+        currentnode, // 当前节点
+        availablenextnodes, // 可用的下一个节点列表
     } = options;
     if (availablenextnodes.length === 0) {
         throw Error(
@@ -30,7 +34,6 @@ export function picknextnodeRoulette(
             const weight = calc_state_transition_probabilities({
                 ...options,
                 getpheromone,
-
                 nextnode,
                 currentnode,
                 alpha,
