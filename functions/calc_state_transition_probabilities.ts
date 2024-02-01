27ceef1e550375eb84f928ab2ed1128b084796d8
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-01 18:32:20 +0800

    Update calc_state_transition_probabilities.ts

--- functions/calc_state_transition_probabilities.ts
+++ functions/calc_state_transition_probabilities.ts
@@ -2,6 +2,17 @@ import { assert_true } from "../test/assert_true";
 import { GetDistanceBySerialNumber } from "./GetDistanceBySerialNumber";
 import { GetPheromone } from "./GetPheromone";
 
+/**
+ * 计算状态转换概率
+ * @param {Object} options - 参数对象
+ * @param {GetPheromone} options.getpheromone - 获取pheromone的方法
+ * @param {number} options.nextnode - 下一个节点的编号
+ * @param {number} options.currentnode - 当前节点的编号
+ * @param {number} options.alpha - alpha参数
+ * @param {GetDistanceBySerialNumber} options.getdistancebyserialnumber - 根据序列号获取距离的方法
+ * @param {number} options.beta - beta参数
+ * @returns {number} - 状态转换概率
+ */
 export function calc_state_transition_probabilities({
     getpheromone,
     nextnode,
