b1be6e957ce8d9595e7db2cd191e453dd2eb7df6
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-02 16:49:46 +0800

    Update thread_Greedy_algorithm_to_solve_tsp_with_selected_start.ts

--- src/thread_Greedy_algorithm_to_solve_tsp_with_selected_start.ts
+++ src/thread_Greedy_algorithm_to_solve_tsp_with_selected_start.ts
@@ -2,12 +2,18 @@ import { GreedyWithStartOptions } from "../functions/GreedyWithStartOptions";
 import { Greedy_algorithm_to_solve_tsp_with_selected_start } from "../functions/Greedy_algorithm_to_solve_tsp_with_selected_start";
 // import { Greedy_algorithm_to_solve_tsp_with_selected_start_pool } from "./Greedy_algorithm_to_solve_tsp_with_selected_start_pool";
 
+/**
+ * 使用贪婪算法解决旅行商问题，并选择指定的起始点。
+ * @param {GreedyWithStartOptions} options - 贪婪算法的选项
+ * @returns {Promise<{ route: number[], length: number }>} - 返回旅行商问题的路径和长度
+ */
 export async function thread_Greedy_algorithm_to_solve_tsp_with_selected_start({
     node_coordinates,
     // start = get_random_start(node_coordinates),
     start,
     end,
     gridDistanceMatrix,
+    ...opts
     // round = false,
     // max_cities_of_greedy = Infinity,
 }: GreedyWithStartOptions): Promise<{
@@ -23,6 +29,7 @@ export async function thread_Greedy_algorithm_to_solve_tsp_with_selected_start({
         start,
         end,
         gridDistanceMatrix,
+        ...opts,
         // round,
         // max_cities_of_greedy,
         // });
