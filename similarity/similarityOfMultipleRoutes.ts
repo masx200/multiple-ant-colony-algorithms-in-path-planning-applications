7d349007b8edb45d55f6a8cba15592d63ef3afd7
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-02 20:01:37 +0800

    similarityOfTwoRoutes

--- similarity/similarityOfMultipleRoutes.ts
+++ similarity/similarityOfMultipleRoutes.ts
@@ -1,11 +1,19 @@
-import { assert_true } from "../test/assert_true";
+// import { assert_true } from "../test/assert_true";
 import { similarityOfTwoRoutes } from "./similarityOfTwoRoutes";
 
+/**
+ * 计算多个路由与最优路由的相似度
+ * @param routes - 路由数组，每个路由为一个数字数组
+ * @param bestRoute - 最优路由，数字数组
+ * @returns - 多个路由与最优路由的相似度
+ */
 export function similarityOfMultipleRoutes(
     routes: number[][],
     bestRoute: number[],
 ): number {
-    assert_true(routes.every((r) => r.length === bestRoute.length));
+    // 确保每个路由的长度与最优路由的长度相同
+    // assert_true(routes.every((r) => r.length === bestRoute.length));
+    // 计算多个路由与最优路由的相似度
     return (
         routes.reduce(
             (p, route) => p + similarityOfTwoRoutes(route, bestRoute),
--- similarity/similarityOfTwoRoutes.ts
+++ similarity/similarityOfTwoRoutes.ts
@@ -1,12 +1,17 @@
 import { not_cycle_route_to_segments } from "../functions/not_cycle_route_to_segments";
 import { assert_true } from "../test/assert_true";
-
+/**
+ * 计算两条路径的相似度
+ * @param route1 第一条路径
+ * @param route2 第二条路径
+ * @returns 相似度
+ */
 export function similarityOfTwoRoutes(
     route1: number[],
     route2: number[],
 ): number {
     assert_true(route1.length > 0);
-    assert_true(route1.length === route2.length);
+    // assert_true(route1.length === route2.length);
     const A: number[][] = Array(route1.length)
         .fill(0)
         .map(() => Array(route1.length).fill(0));
