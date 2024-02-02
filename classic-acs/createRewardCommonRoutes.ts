f1321f3eddd011c8a44030544687d53755a78f8c
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-02 21:00:04 +0800

    Update createRewardCommonRoutes.ts

--- classic-acs/createRewardCommonRoutes.ts
+++ classic-acs/createRewardCommonRoutes.ts
@@ -2,6 +2,13 @@ import { max } from "lodash-es";
 
 import { MatrixSymmetry } from "@masx200/sparse-2d-matrix";
 
+/**
+ * 创建奖励通用路由的函数
+ * @param pheromone_volatilization_coefficient_of_communication - 蜂群挥发系数
+ * @param pheromoneStore - 蜂群存储矩阵
+ * @param count_of_nodes - 节点数量
+ * @returns {function} - 奖励通用路由的函数
+ */
 export function createRewardCommonRoutes(
     pheromone_volatilization_coefficient_of_communication: number,
     pheromoneStore: MatrixSymmetry<number>,
@@ -13,6 +20,7 @@ export function createRewardCommonRoutes(
         for (let i = 0; i < n; i++)
             for (let j = i; j < n; j++) {
                 if (i !== j) {
+                    common[i] ??= [];
                     if (common[i][j] > 0) {
                         const value =
                             (1 -
