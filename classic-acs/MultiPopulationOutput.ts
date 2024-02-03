5e34b1da4245b5529e6e6129bdd04fd980768512
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-03 21:55:06 +0800

    添加注释

--- classic-acs/MultiPopulationOutput.ts
+++ classic-acs/MultiPopulationOutput.ts
@@ -1,11 +1,23 @@
 import { COMMON_DataOfOneIteration, COMMON_TSP_Output } from "./tsp-interface";
 
 // import { DataOfFinishOneRoute } from "../functions/DataOfFinishOneRoute";
-
+/**
+ * 多种人口输出类型
+ */
 export type MultiPopulationOutput = COMMON_TSP_Output & {
+    /**
+     * 所有种群相似度的历史记录
+     */
     similarityOfAllPopulationsHistory: number[];
+    /**
+     * 各个种群在各代中的迭代数据
+     */
     IterationDataOfIndividualPopulations: COMMON_DataOfOneIteration[][];
     // RouteDataOfIndividualPopulations: DataOfFinishOneRoute[][];
+    /**
+     * 各个种群在各代中的最佳路径更新历史记录
+     */
+    // HistoryOfPopulationsAllUpdateBestRoute: boolean[];
     HistoryOfTheWayPopulationsCommunicate: string[];
     HistoryOfPopulationsAllUpdateBestRoute: boolean[];
 };
--- classic-acs/MultiPopulationSchedulerRemote.ts
+++ classic-acs/MultiPopulationSchedulerRemote.ts
@@ -1,9 +1,11 @@
 import { Remote } from "comlink";
 
 import { MultiPopulationSchedulerAPI } from "./MultiPopulationSchedulerAPI";
-
+/**
+ * 多种人口调度器的远程接口
+ */
 export interface MultiPopulationSchedulerRemote {
-    remote: Remote<MultiPopulationSchedulerAPI>;
-    worker: Worker;
-    terminate: () => void;
+    remote: Remote<MultiPopulationSchedulerAPI>; // 远程连接对象
+    worker: Worker; // 工作线程对象
+    terminate: () => void; // 终止函数
 }
