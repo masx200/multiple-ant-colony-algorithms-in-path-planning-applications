267cc1dce820af14976ef3f9f7f68b1625d60095
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-03 21:46:26 +0800

    Update TSP_Worker_API.ts

--- src/TSP_Worker_API.ts
+++ src/TSP_Worker_API.ts
@@ -1,6 +1,13 @@
 import { RunnerMultipleCommunicative } from "./RunnerMultipleCommunicative";
 import { TSPRunnerOptions } from "./TSPRunnerOptions";
-
+/**
+ * TSP_Worker_API类型定义
+ */
 export type TSP_Worker_API = RunnerMultipleCommunicative & {
+    /**
+     * 初始化运行器
+     * @param options 运行器选项
+     * @returns 返回一个Promise对象，表示初始化操作的异步结果
+     */
     init_runner: (options: TSPRunnerOptions) => Promise<void>;
 };
