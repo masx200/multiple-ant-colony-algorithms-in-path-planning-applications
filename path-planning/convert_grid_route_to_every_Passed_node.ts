92fbf33ad05a2b68a6be5613a648f376176d9201
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-16 13:11:22 +0800

    Update convert_grid_route_to_every_Passed_node.ts

--- path-planning/convert_grid_route_to_every_Passed_node.ts
+++ path-planning/convert_grid_route_to_every_Passed_node.ts
@@ -1,3 +1,6 @@
+import { not_cycle_route_to_segments } from "../functions/not_cycle_route_to_segments";
+import { getPathCoordinates } from "./getPathCoordinates";
+
 /**
  * 将网格路由转换为每个经过的节点的数组
  * @param route 网格路由数组，每个元素为一个二维数组，表示一个节点的行列坐标
@@ -6,5 +9,9 @@
 export function convert_grid_route_to_every_Passed_node(
     route: [number, number][],
 ): [number, number][] {
-    return route;
+    const segments = not_cycle_route_to_segments(route);
+    const nodes = segments
+        .map((segment) => getPathCoordinates(segment[0], segment[1]))
+        .flat();
+    return nodes;
 }
