6d1c08f6361b8559d9758f8180305460bd29a7ec
masx200 <34191203+masx200@users.noreply.github.com>
2024-02-16 14:06:55 +0800

    Update generate_local_optimization_grid_routes.ts

--- path-planning/generate_local_optimization_grid_routes.ts
+++ path-planning/generate_local_optimization_grid_routes.ts
@@ -1,5 +1,6 @@
 import { visibleGridsMatrixCallBack } from "./visibleGridsMatrixCallBack";
 import { combinations } from "combinatorial-generators";
+import { convert_grid_route_to_every_Passed_node } from "./convert_grid_route_to_every_Passed_node";
 /**
  * 生成局部优化网格路径的路由
  * @param route 路径数组
@@ -10,11 +11,12 @@ export function generate_local_optimization_grid_routes(
     route: [number, number][],
     canStraightReach: visibleGridsMatrixCallBack,
 ): [number, number][][] {
-    const sequences = [...combinations(route.keys(), 2)];
+    const every_nodes = convert_grid_route_to_every_Passed_node(route);
+    const sequences = [...combinations(every_nodes.keys(), 2)];
     const result: [number, number][][] = [];
     for (const [a, b] of sequences) {
-        const point1 = route[a];
-        const point2 = route[b];
+        const point1 = every_nodes[a];
+        const point2 = every_nodes[b];
         if (canStraightReach(point1, point2)) {
         }
     }
